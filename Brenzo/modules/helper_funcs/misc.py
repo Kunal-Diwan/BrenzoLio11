716ed603e5cf8a56185cb90226f0c88a0799ae0b
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-08-17 12:49:58 +0530

    Try to changed help mod

--- Brenzo/modules/helper_funcs/misc.py
+++ Brenzo/modules/helper_funcs/misc.py
@@ -41,49 +41,42 @@ def split_message(msg: str) -> List[str]:
         return result
 
 
-def paginate_modules(chat_id,
-                     page_n: int,
-                     module_dict: Dict,
-                     prefix,
-                     chat=None) -> List:
+def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
-        modules = sorted([
-            EqInlineKeyboardButton(tld(chat_id, "modname_" + x),
-                                   callback_data="{}_module({})".format(
-                                       prefix, x)) for x in module_dict.keys()
-        ])
+        modules = sorted(
+            [EqInlineKeyboardButton(x.__mod_name__,
+                                    callback_data="{}_module({})".format(prefix, x.__mod_name__.lower())) for x
+             in module_dict.values()])
     else:
-        modules = sorted([
-            EqInlineKeyboardButton(tld(chat_id, "modname_" + x),
-                                   callback_data="{}_module({},{})".format(
-                                       prefix, chat, x))
-            for x in module_dict.keys()
-        ])
+        modules = sorted(
+            [EqInlineKeyboardButton(x.__mod_name__,
+                                    callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
+             in module_dict.values()])
 
-    pairs = list(zip(modules[::2], modules[1::2]))
 
-    if len(modules) % 2 == 1:
-        pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) / 7)
-    modulo_page = page_n % max_num_pages
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc == 1:
+        pairs.append((modules[-1],))
+    elif calc == 2:
+        pairs.append((modules[-1],))
+
+
+#    pairs = list(zip(modules[::2], modules[1::2]))
+
+#    if len(modules) % 2 == 1:
+#        pairs.append((modules[-1],))
+
+#    max_num_pages = ceil(len(pairs) / 7)
+#   modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 50:
-        pairs = pairs[modulo_page * 50:50 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton(
-                "<<", callback_data="{}_prev({})".format(prefix, modulo_page)),
-             EqInlineKeyboardButton(tld(chat_id, 'btn_go_back'),
-                                    callback_data="bot_start"),
-             EqInlineKeyboardButton(">>",
-                                    callback_data="{}_next({})".format(
-                                        prefix, modulo_page)))
-        ]
-    else:
-        pairs += [[
-            EqInlineKeyboardButton(tld(chat_id, 'btn_go_back'),
-                                   callback_data="bot_start")
-        ]]
+#    if len(pairs) > 7:
+#        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
+#            (EqInlineKeyboardButton("<", callback_data="{}_prev({})".format(prefix, modulo_page)),
+#             EqInlineKeyboardButton(">", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     return pairs
 
