00a623f4e5f77f5d3e07011b45466c74548e7e91
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-08-21 12:22:09 +0530

    Update misc.py

--- Brenzo/modules/helper_funcs/misc.py
+++ Brenzo/modules/helper_funcs/misc.py
@@ -60,17 +60,17 @@ def paginate_modules(chat_id,
             for x in module_dict.keys()
         ])
 
-    pairs = list(zip(modules[::2], modules[1::2]))
+    pairs = list(zip(modules[::2], modules[1::3]))
 
-    if len(modules) % 2 == 1:
+    if len(modules) % 3 == 1:
         pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) / 7)
+    max_num_pages = ceil(len(pairs) / 50)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 7:
-        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
+    if len(pairs) > 50:
+        pairs = pairs[modulo_page * 50:50 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton(
                 "<<", callback_data="{}_prev({})".format(prefix, modulo_page)),
              EqInlineKeyboardButton(tld(chat_id, 'btn_go_back'),
