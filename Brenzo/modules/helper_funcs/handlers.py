31bc351a3bda38f7331ee43d39de95e23c3622a8
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-08-24 20:05:05 +0530

    Update handlers.py

--- Brenzo/modules/helper_funcs/handlers.py
+++ Brenzo/modules/helper_funcs/handlers.py
@@ -2,7 +2,7 @@ import telegram.ext as tg
 from telegram import Update
 import Brenzo.modules.sql.antispam_sql as sql
 
-CMD_STARTERS = ('/', '!', '.', '*')
+CMD_STARTERS = ('/', '!', '.', ',')
 
 
 class CustomCommandHandler(tg.CommandHandler):
