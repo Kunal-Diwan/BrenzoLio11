6bcc4d98b41bf4d445c7a02808552b1daa4144b3
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-08-24 10:50:10 +0530

    Update __init__.py

--- Brenzo/__init__.py
+++ Brenzo/__init__.py
@@ -1,133 +1,113 @@
+#    Haruka Aya (A telegram bot project)
+#    Copyright (C) 2017-2019 Paul Larsen
+#    Copyright (C) 2019-2021 Akito Mizukito (Haruka Aita)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import logging
-import os
 import sys
+import yaml
 
-import telegram.ext as tg
-from googletrans import Translator
-from pyrogram import Client
 from telethon import TelegramClient
+import telegram.ext as tg
 
-# enable logging
+#Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO)
 
 LOGGER = logging.getLogger(__name__)
 
-# if version < 3.6, stop bot.
-if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
+LOGGER.info("Starting brenzo...")
+
+# If Python version is < 3.6, stops the bot.
+if sys.version_info[0] < 3 or sys.version_info[1] < 8:
+    LOGGER.error(
+        "You MUST have a python version of at least 3.8! Multiple features depend on this. Bot quitting."
+    )
     quit(1)
 
-ENV = bool(os.environ.get('ENV', False))
-
-if ENV:
-    TOKEN = os.environ.get('TOKEN', None)
-    API_KEY = os.environ.get('API_KEY', None)
-    API_HASH = os.environ.get('API_HASH', None)
-    try:
-        OWNER_ID = int(os.environ.get('OWNER_ID', None))
-    except ValueError:
-        raise Exception("Your OWNER_ID env variable is not a valid integer.")
-
-    MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
-
-    try:
-        SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
-    except ValueError:
-        raise Exception("Your sudo users list does not contain valid integers.")
-
-    try:
-        SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "").split())
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    WEBHOOK = bool(os.environ.get('WEBHOOK', False))
-    URL = os.environ.get('URL', "")  # Does not contain token
-    PORT = int(os.environ.get('PORT', 5000))
-    CERT_PATH = os.environ.get("CERT_PATH")
-
-    DB_URI = os.environ.get('DATABASE_URL')
-    LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
-    DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
-    STRICT_ANTISPAM = bool(os.environ.get('STRICT_GBAN', False))
-    WORKERS = int(os.environ.get('WORKERS', 8))
-    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
-    ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
-    API_WEATHER = os.environ.get('API_WEATHER', None)
-
-
-else:
-    from Brenzo.config import Development as Config
-    TOKEN = Config.API_KEY
-    try:
-        OWNER_ID = int(Config.OWNER_ID)
-    except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
-
-    MESSAGE_DUMP = Config.MESSAGE_DUMP
-    OWNER_USERNAME = Config.OWNER_USERNAME
-
-    try:
-        SUDO_USERS = set(int(x) for x in Config.SUDO_USERS or [])
-    except ValueError:
-        raise Exception("Your sudo users list does not contain valid integers.")
-
-    try:
-        SUPPORT_USERS = set(int(x) for x in Config.SUPPORT_USERS or [])
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        WHITELIST_USERS = set(int(x) for x in Config.WHITELIST_USERS or [])
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    WEBHOOK = bool(os.environ.get('WEBHOOK', False))
-    URL = os.environ.get('URL', "")  # Does not contain token
-    PORT = int(os.environ.get('PORT', 5000))
-    CERT_PATH = os.environ.get("CERT_PATH")
-
-    DB_URI = os.environ.get('DATABASE_URL')
-    LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
-    DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
-    STRICT_ANTISPAM = bool(os.environ.get('STRICT_GBAN', False))
-    WORKERS = int(os.environ.get('WORKERS', 8))
-    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
-    ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
-    API_WEATHER = os.environ.get('API_WEATHER', None)
-
-# OpenWeather
+# Load config
 try:
-    WEATHER_API = os.environ.get('WEATHER_API')
-except ValueError:
-    raise Exception("Your 'WEATHER_API' variable is not a valid integer.")
+    CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
+except FileNotFoundError:
+    print("Are you dumb? C'mon start using your brain!")
+    quit(1)
+except Exception as eee:
+    print(
+        f"Ah, look like there's error(s) while trying to load your config. It is\n!!!! ERROR BELOW !!!!\n {eee} \n !!! ERROR END !!!"
+    )
+    quit(1)
 
+if not CONFIG['is_example_config_or_not'] == "not_sample_anymore":
+    print("Please, use your eyes and stop being blinded.")
+    quit(1)
+
+TOKEN = CONFIG['bot_token']
+API_KEY = CONFIG['api_key']
+API_HASH = CONFIG['api_hash']
+
+try:
+    OWNER_ID = int(CONFIG['owner_id'])
+except ValueError:
+    raise Exception("Your 'owner_id' variable is not a valid integer.")
+
+try:
+    MESSAGE_DUMP = CONFIG['message_dump']
+except ValueError:
+    raise Exception("Your 'message_dump' must be set.")
+
+try:
+    OWNER_USERNAME = CONFIG['owner_username']
+except ValueError:
+    raise Exception("Your 'owner_username' must be set.")
+
+try:
+    SUDO_USERS = set(int(x) for x in CONFIG['sudo_users'] or [])
+except ValueError:
+    raise Exception("Your sudo users list does not contain valid integers.")
+
+try:
+    WHITELIST_USERS = set(int(x) for x in CONFIG['whitelist_users'] or [])
+except ValueError:
+    raise Exception(
+        "Your whitelisted users list does not contain valid integers.")
+
+DB_URI = CONFIG['database_url']
+LOAD = CONFIG['load']
+NO_LOAD = CONFIG['no_load']
+DEL_CMDS = CONFIG['del_cmds']
+STRICT_ANTISPAM = CONFIG['strict_antispam']
+WORKERS = CONFIG['workers']
+
+# Append OWNER_ID to SUDO_USERS
 SUDO_USERS.add(OWNER_ID)
-SUDO_USERS.add(254318997) #paul's id
+
+# SpamWatch
+spamwatch_api = CONFIG['sw_api']
+
+if spamwatch_api == "None":
+    spamwatch_api == None
 
 updater = tg.Updater(TOKEN, workers=WORKERS)
-dispatcher = updater.dispatcher
 
-trl = Translator()
+dispatcher = updater.dispatcher
 
 tbot = TelegramClient("Brenzo", API_KEY, API_HASH)
 
-pbot = Client("BrenzoPyro", api_id=API_KEY,
-              api_hash=API_HASH,
-              bot_token=TOKEN)
-
 SUDO_USERS = list(SUDO_USERS)
 WHITELIST_USERS = list(WHITELIST_USERS)
-SUPPORT_USERS = list(SUPPORT_USERS)
 
 # Load at end to ensure all prev variables have been set
 from Brenzo.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler
@@ -135,5 +115,4 @@ from Brenzo.modules.helper_funcs.handlers import CustomCommandHandler, CustomReg
 # make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
 
-if ALLOW_EXCL:
-    tg.CommandHandler = CustomCommandHandler
+tg.CommandHandler = CustomCommandHandler
