6714f4e5b01ce27f81a0d8f07fe8a8536d21e6c4
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-09-20 12:46:44 +0530

    Update feedback.py

--- Brenzo/modules/feedback.py
+++ Brenzo/modules/feedback.py
@@ -19,7 +19,7 @@ def feedback(bot: Bot, update: Update):
   feed_text = f"Brenzo's *New* feedback from [{name}](tg://user?id={userid})\n\nfeed: {text}"
   
 
-  bot.send_message(-1001296653212, feed_text, parse_mode=ParseMode.MARKDOWN)
+  bot.send_message(-1001513232985, feed_text, parse_mode=ParseMode.MARKDOWN)
  
   text = html.escape(text)
   reply_text=f"Thankyou for giving us your feedback."
