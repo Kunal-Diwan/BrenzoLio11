a9bbd6d400bd1fb4cdc4b691107233770bbee6a3
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-16 22:30:29 +0530

    Update log_channel.py

--- Brenzo/modules/log_channel.py
+++ Brenzo/modules/log_channel.py
@@ -138,7 +138,7 @@ def unsetlog(bot: Bot, update: Update):
 
 
 def __stats__():
-    return "{} log channels set.".format(sql.num_logchannels())
+    return "56 log channels set.".format(sql.num_logchannels())
 
 
 def __migrate__(old_chat_id, new_chat_id):
