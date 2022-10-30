9b2f78448587ae94dcb9844f9a49e4ac0ef3251d
Kunal Diwan <kunaldiwan1405@gmail.com>
2022-10-30 13:46:07 +0530

    Create config.py

---
+++ Brenzo/config.py
@@ -0,0 +1,42 @@
+class Config(object):
+    LOGGER = True
+
+    # REQUIRED
+    API_KEY = "3201985"
+    API_HASH = "41ae80228bc4eff613d987a2953cb139"
+    OWNER_ID = "1701601729"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "Kdiwan"
+
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    MESSAGE_DUMP = "-1001592735448"  # needed to make sure 'save from' messages persist
+    LOAD = []
+    NO_LOAD = ['translation', 'sed']
+    WEBHOOK = False
+    URL = None
+
+    # OPTIONAL
+    SUDO_USERS = [1701601729 1833703265 18561438011701601729 1833703265 18561438011701601729 1833703265 1856143801]  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = [1701601729 1833703265 18561438011701601729 1833703265 1856143801]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = [1701601729 1833703265 1856143801]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    MAPS_API = ''
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
+    STRICT_ANTISPAM = False
+    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
+    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
+    STRICT_GBAN = False
+    STRICT_GMUTE = False
+    ALLOW_EXCL = True  # Allow ! commands as well as /
+    API_OPENWEATHER = None # OpenWeather API
+
+    # MEMES
+    DEEPFRY_TOKEN = None
+
+class Production(Config):
+    LOGGER = False
+
+
+class Development(Config):
+    LOGGER = True
